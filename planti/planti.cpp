21c20b34fd8cc4329f3781785c4ecc79c5dee5a8
Marcela Tovar <marcelatovar.flores@gmail.com>
2023-05-29 18:20:15 -0600

    Primer commir

---
+++ planti/planti.cpp
@@ -0,0 +1,10 @@
+#include "Tipos.h"
+#include "Pila.h"
+#include <iostream>
+#include <string>
+#include "MiClase.h"
+using namespace std;
+int main() {
+	std::cout << "\n\nEnteros:\n";
+	Tipos<int>();
+}
